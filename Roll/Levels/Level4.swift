4683ebdd5127abb46c46cfc52c5145035601a97d
Greg Grant <ggrant005@gmail.com>
2018-03-17 12:01:02 -0700

    Add fourth level.

---
+++ Roll/Levels/Level4.swift
@@ -0,0 +1,25 @@
+//
+//  Level3.swift
+//  Roll
+//
+//  Created by Greg Grant on 3/11/18.
+//  Copyright © 2018 Greg Grant. All rights reserved.
+//
+
+import SpriteKit
+
+class Level4 : Level {
+  override init() {
+    super.init()
+    mLevelOptions.mBallOptions = BallOptions()
+    mLevelOptions.mBallOptions.mIsDynamic = false
+    mLevelOptions.mBallOptions.mStartPosition = CGPoint(x: -200, y: 300)
+    
+    mLevelOptions.mBlockOptions = BlockOptions()
+    mLevelOptions.mBlockOptions.mStartPosition = CGPoint(x: 50, y: -300)
+    mLevelOptions.mBlockOptions.mMovement = .mSpin(0.5)
+    
+    mLevelOptions.mGoalOptions = GoalOptions()
+    mLevelOptions.mGoalOptions.mStartPosition = CGPoint(x: 100, y: -350)
+  }
+}
